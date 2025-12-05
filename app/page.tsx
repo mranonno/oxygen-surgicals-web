c70117c126f11b240567af962b56244593f16d42
Anonno <anonnodas97@gmail.com>
2025-12-06 01:31:21 +0600

    spelling ignore

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import Footer from "@/components/common/Footer";
-import Navbar from "@/components/common/Navbar";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen  flex-col items-center justify-between py-32 font-sans bg-zinc-50 sm:items-start">
-      <Navbar />
-      <Footer />
-    </main>
+    <div className="w-full text-center">
+      <h1 className="text-3xl font-bold">Welcome to Oxygen Surgicals</h1>
+    </div>
   );
 }
