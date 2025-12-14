3df6a88eacecfee37ddea47164f4eac26315ec48
Anonno <anonnodas97@gmail.com>
2025-12-15 00:40:04 +0600

    create initial slider

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,47 @@
-import Slider from "@/components/ui/slider/Slider";
+"use client";
+
+import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 export default function Home() {
-  const images = [
-    "https://picsum.photos/200/300",
-    "https://picsum.photos/200/300",
-  ];
   return (
-    <div className="w-full text-center mt-3 items-center">
-      <Slider images={images} />
-      <h1 className="text-3xl h-50 font-bold">Welcome to Oxygen Surgicals</h1>
+    <div className="w-full text-center mt-3">
+      <Swiper
+        modules={[Pagination, Navigation]}
+        pagination={{ clickable: true }}
+        navigation
+        className="rounded-2xl overflow-hidden"
+      >
+        <SwiperSlide>
+          <div className="relative w-full h-[520px]">
+            <Image
+              src="/banner/slider_1.jpg"
+              alt="banner"
+              fill
+              className="object-cover"
+              priority
+            />
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="relative w-full h-[520px]">
+            <Image
+              src="/banner/slider_1.jpg"
+              alt="banner"
+              fill
+              className="object-cover"
+            />
+          </div>
+        </SwiperSlide>
+      </Swiper>
+
+      <h1 className="text-3xl font-bold mt-4">Welcome to Oxygen Surgicals</h1>
     </div>
   );
 }
